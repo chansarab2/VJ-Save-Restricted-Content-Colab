581044bff8b4549f4b85c780934722ff302ceb0e
chansarab2 <chanpreet2424tgt@gmail.com>
2025-06-20 02:21:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "28908355"))
 API_HASH = os.environ.get("API_HASH", "d7f53de270d2ef1ad9754fc22f2ced5a")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "mongodb+srv://chanpreet2424tgt:FtXUmhmYOfeit3t7@cluster0.tkxto8d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://chanpreet151515:<db_password>@cluster0.uzvrequ.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
